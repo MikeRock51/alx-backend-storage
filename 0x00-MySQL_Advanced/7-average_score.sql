14c8ac27e6c4a0f16f6b79f4f51f93ba78f6950f
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-14 19:38:07 +0100

    Update Task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,12 +5,13 @@
     -- Procedure ComputeAverageScoreForUser is taking 1 input:
     -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 
-
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average DECIMAL(10, 2);
-    SELECT AVG(score) INTO average FROM corrections WHERE user_id = user_id;
-    UPDATE users SET average_score = average WHERE id = user_id;
+    -- DECLARE average DECIMAL(10, 2);
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
 END$$
 DELIMITER ;
