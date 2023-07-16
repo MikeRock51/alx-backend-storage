1500553457a23910ed60b4438bc9b3f483c48a41
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-16 05:49:49 +0100

    Add Task 12...

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,9 @@
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUserthat computes and
+-- store the average weighted score for a student.
+-- Requirements:
+	-- Procedure ComputeAverageScoreForUser is taking 1 input:
+	-- user_id, a users.id value (you can assume user_id
+	-- is linked to an existing users)
+
+
