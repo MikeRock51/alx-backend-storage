4244b75adaf9e1895a8095b85324f7016442ed23
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-16 07:45:55 +0100

    Add Task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,4 +6,23 @@
 	-- user_id, a users.id value (you can assume user_id
 	-- is linked to an existing users)
 
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE totalWeight INT;
+	DECLARE totalScore INT;
 
+	SELECT SUM(corrections.score * projects.weight) INTO totalScore FROM corrections
+	INNER JOIN projects ON projects.id = corrections.project_id
+	WHERE corrections.user_id = user_id;
+
+	SELECT SUM(weight) INTO totalWeight FROM corrections
+	INNER JOIN projects ON corrections.project_id = projects.id
+	WHERE corrections.user_id = user_id;
+
+	UPDATE users
+	SET average_score = totalScore / totalWeight
+	WHERE users.id = user_id;
+END$$
+DELIMITER ;
