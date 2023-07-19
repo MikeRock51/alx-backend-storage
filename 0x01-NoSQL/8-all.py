e80e31ab70b3ceda77b375c0379089547e2be1ed
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-18 22:03:24 -0700

    Add Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """Access mongodb via pymongo"""
 
-from pymongo import MongoClient
-
 
 def list_all(mongo_collection):
     """Lists all documents in a mongodb collection"""
 
-    client = MongoClient('mongodb://localhost:27017')
+    docs = mongo_collection.find()
+
+    if not docs:
+        return []
+
+    return docs
