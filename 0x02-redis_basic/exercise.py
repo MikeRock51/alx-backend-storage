ca9627c7499cfe50bcd16577f0cd22fceb544cab
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-20 08:32:39 +0100

    Update redis attr

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,13 +11,13 @@ class Cache:
 
     def __init__(self):
         """Stores a Redis instance"""
-        self.__redis = Redis()
-        self.__redis.flushdb()
+        self._redis = Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in redis and returns the ID"""
 
         id: str = str(uuid4())
-        self.__redis.set(id, data)
+        self._redis.set(id, data)
 
         return id
