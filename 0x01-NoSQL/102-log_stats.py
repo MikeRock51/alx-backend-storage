5d15e1e55cc0877ee686da556e7f04d884e9736e
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-19 22:10:03 -0700

    Remove dump file

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,18 +1,53 @@
 #!/usr/bin/env python3
 """
-    Write a Python function that returns all students
-    sorted by average score:
-        Prototype: def top_students(mongo_collection):
-        mongo_collection will be the pymongo collection object
-        The top must be ordered
-        The average score must be part of each item
-        returns with key = averageScore
+    Write a Python script that provides some stats
+    about Nginx logs stored in MongoDB:
+        Database: logs
+        Collection: nginx
+        Display (same as the example):
+        first line: x logs where x is the number of
+        documents in this collection
+        second line: Methods:
+        5 lines with the number of documents with the
+        method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        in this order (see example below - warning: it’s a
+        tabulation before each line)
+        one line with the number of documents with:
+            method=GET
+            path=/status
 """
 
+from pymongo import MongoClient
 
-def top_students(mongo_collection):
-    """Returns all student sorted by average score"""
 
-    students = mongo_collection.find()
+if __name__ == '__main__':
 
-    print(lst(students))
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    logCount = collection.count_documents({})
+    getMethodCount = collection.count_documents({'method': 'GET'})
+    postMethodCount = collection.count_documents({'method': 'POST'})
+    putMethodCount = collection.count_documents({'method': 'PUT'})
+    patchMethodCount = collection.count_documents({'method': 'PATCH'})
+    deleteMethodCount = collection.count_documents({'method': 'DELETE'})
+
+    statusGetCount = collection.count_documents(
+            {'method': 'GET', 'path': '/status'}
+    )
+
+    ips = sorted(collection.distinct("ip"), reverse=True)[:10]
+
+    print("{} logs".format(logCount))
+    print("Methods:")
+    print("\tmethod GET: {}".format(getMethodCount))
+    print("\tmethod POST: {}".format(postMethodCount))
+    print("\tmethod PUT: {}".format(putMethodCount))
+    print("\tmethod PATCH: {}".format(patchMethodCount))
+    print("\tmethod DELETE: {}".format(deleteMethodCount))
+    print("{} status check".format(statusGetCount))
+    
+    print('IPS:')
+    for ip in ips:
+        print("\t{}: {}".format(ip,
+            collection.count_documents({"ip": ip})))
