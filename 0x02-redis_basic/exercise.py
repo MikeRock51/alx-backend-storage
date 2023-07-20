3323abf115ce9fcd538e27b7cef7283536ef5864
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-20 08:30:30 +0100

    Privatise redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,13 +11,13 @@ class Cache:
 
     def __init__(self):
         """Stores a Redis instance"""
-        self._redis = Redis()
-        self._redis.flushdb()
+        self.__redis = Redis()
+        self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in redis and returns the ID"""
 
         id: str = str(uuid4())
-        self._redis.set(id, data)
+        self.__redis.set(id, data)
 
         return id
