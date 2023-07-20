c8e33a67ce36dccd4b02f33e2b0bf2460a41d3ff
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-20 08:28:20 +0100

    Add Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,22 @@
 """Exploring Redis basics/ Redis-py"""
 
 from redis import Redis
+from uuid import uuid4
+from typing import Union
 
 
-Class 
+class Cache:
+    """Caching with Redis"""
+
+    def __init__(self):
+        """Stores a Redis instance"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores data in redis and returns the ID"""
+
+        id: str = str(uuid4())
+        self._redis.set(id, data)
+
+        return id
